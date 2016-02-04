03072db454fce7f2c31ddb5bc5cc4090d6be45a8
Ryan Wheale <ryan.wheale@gmail.com>
2016-02-03 17:55:51 -0700

    Fix issue with waitFor and failed promises

--- extensions/can-ssr-app-map.js
+++ extensions/can-ssr-app-map.js
@@ -7,22 +7,38 @@ AppMap.prototype.setup = function () {
 	this.__readyPromise = can.Deferred();
 };
 
+AppMap.prototype.__pluckPromise = function (promise) {
+	var pIndex = this.__readyPromises.indexOf(promise);
+	if (pIndex > -1) {
+		this.__readyPromises.splice(pIndex, 1);
+	}
+
+	if (this.__readyPromises.length === 0) {
+		setTimeout(() => {
+			if (this.__readyPromises.length === 0) {
+				this.__readyPromise.resolve();
+			}
+		});
+	}
+}
+
 AppMap.prototype.waitFor = function(promise) {
 	this.__readyPromises.push(promise);
 
+	//!steal-remove-start
+	// Clever little way of getting the module path and line 
+	// number where this promise is defined (3rd line of stack).
+	var err = new Error();
+	var parentFile = err.stack.split(/[\r\n]/)[2].split("/").slice(1).join("/");
+	//!steal-remove-end
+	
 	promise.then(() => {
-		var pIndex = this.__readyPromises.indexOf(promise);
-		if (pIndex > -1) {
-			this.__readyPromises.splice(pIndex, 1);
-		}
-
-		if (this.__readyPromises.length === 0) {
-			setTimeout(() => {
-				if (this.__readyPromises.length === 0) {
-					this.__readyPromise.resolve();
-				}
-			});
-		}
+		this.__pluckPromise(promise);
+	}, (err) => {
+		//!steal-remove-start
+		console.log("༼ つ ◕_◕ ༽つ ~~~ A promise has failed to hold up its end of the bargain!!! \nPromise source:", parentFile, "\nERROR:", err);
+		//!steal-remove-end
+		this.__pluckPromise(promise);
 	});
 
 	return promise;
